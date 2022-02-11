307fc2d253f4ad0d773626235895daedc2d74243
pawel83gt <pawel83gt@gmail.com>
2022-02-12 00:02:40 +0700

    Добавьте файлы проекта.

---
+++ ShopUgol/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+//await app.StartAsync();
+//await Task.Delay(10000);
+//await app.StopAsync();
+app.Run();
+
